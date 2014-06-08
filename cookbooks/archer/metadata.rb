92841a12e5ababa0f96d96042852601fe534df86
Brandon Fulk <brandon.fulk@gmail.com>
2014-06-08 00:45:29 -0500

    Create archer cookbook

---
+++ cookbooks/archer/metadata.rb
@@ -0,0 +1,7 @@
+name             'archer'
+maintainer       'Brandon Fulk'
+maintainer_email 'bf@the42.com'
+license          'All rights reserved'
+description      'Installs/Configures archer'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/archer/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: archer
+# Recipe:: default
+#
+# Copyright 2014, Brandon Fulk
+#
+# All rights reserved - Do Not Redistribute
+#
